afba906c1947836bd4f7d8c8eae60e04d1433f2b
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-05 12:32:46 +0000

    JCD1.4.3

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace JCD1._4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Digite um número: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            double numModule = Math.Abs(num);
+            Console.WriteLine("O módulo do número digitado é " + numModule + " ");
+
+
+            //public double SquareRoot ()
+        }
+    }
+}
